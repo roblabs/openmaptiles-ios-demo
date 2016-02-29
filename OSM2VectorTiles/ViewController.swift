78f84ab3b594bbb48919a4c26ad226df9377856e
Petr Pridal <petr.pridal@klokantech.com>
2016-02-29 21:13:44 +0100

    Initial Commit

---
+++ OSM2VectorTiles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OSM2VectorTiles
+//
+//  Created by Petr Pridal on 29/02/16.
+//  Copyright © 2016 Klokan Technologies GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
