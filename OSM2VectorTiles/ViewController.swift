6c61e7337e32052506c64320b9110961fff22857
Petr Pridal <petr.pridal@klokantech.com>
2016-03-03 17:48:46 +0100

    Displaying OSM2Vector Tiles with custom local GL Style and custom assets

--- OSM2VectorTiles/ViewController.swift
+++ OSM2VectorTiles/ViewController.swift
@@ -6,11 +6,19 @@
 //  Copyright © 2016 Klokan Technologies GmbH. All rights reserved.
 //
 
+// https://www.mapbox.com/help/first-steps-ios-sdk/
+
 import UIKit
+import Mapbox
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MGLMapView!
+    
     override func viewDidLoad() {
+        
+        mapView.logoView.hidden = true;
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
