04f600be02aa433627b9fe31854514807df29632
Rob | Labs <roblabs@users.noreply.github.com>
2019-11-21 15:46:42 -0800

    + Add Git branch & Git SHA to log + Xcode app version now includes integration number (Xcode build versions are required to be monotonically increasing)

--- Continuous-Integration/ci.sh
+++ Continuous-Integration/ci.sh
@@ -7,8 +7,14 @@
 export PREBUILD_LOG=${PROJECT_DIR}/prebuild.log
 rm -f $PREBUILD_LOG
 
-# Xcode Server, then use the integration number
-if [ $XCS -eq 1 ]
+# Xcode Pre-build scripts goto stdError, so use this to capture
+exec > $PREBUILD_LOG 2>&1
+
+export LANG=en_US.UTF-8   # to avoid Pod WARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+date
+
+# if Xcode Server, then use the integration number
+if [ "$XCS" == 1 ]
 then
   export PREBUILD_LOG=${PROJECT_DIR}/prebuild.${XCS_BOT_TINY_ID}.${XCS_INTEGRATION_NUMBER}.log
 
@@ -20,18 +26,21 @@ then
   echo XCS_INTEGRATION_TINY_ID = $XCS_INTEGRATION_TINY_ID
   echo XCS_INTEGRATION_NUMBER = $XCS_INTEGRATION_NUMBER
   echo XCS_INTEGRATION_RESULT = $XCS_INTEGRATION_RESULT
-  echo PROJECT_DIR = $PROJECT_DIR
   echo XCS_SOURCE_DIR = $XCS_SOURCE_DIR
   echo XCS_OUTPUT_DIR = $XCS_OUTPUT_DIR
   echo XCS_DERIVED_DATA_DIR = $XCS_DERIVED_DATA_DIR
   echo XCS_XCODEBUILD_LOG = $XCS_XCODEBUILD_LOG
 fi
 
-# Xcode Pre-build scripts goto stdError, so use this to capture
-exec > $PREBUILD_LOG 2>&1
+echo PROJECT_DIR = $PROJECT_DIR
+gitBranch=$(git -C ${PROJECT_DIR} rev-parse --abbrev-ref HEAD)
+echo gitBranch = $gitBranch
+gitSHA=$(git -C ${PROJECT_DIR} rev-parse HEAD)
+echo gitSHA = $gitSHA
 
-export LANG=en_US.UTF-8   # to avoid Pod WARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
-date
+echo CURRENT_PROJECT_VERSION = $CURRENT_PROJECT_VERSION
+echo MARKETING_VERSION = $MARKETING_VERSION
+
 echo DEVELOPMENT_TEAM = $DEVELOPMENT_TEAM
 echo PREBUILD_LOG = $PREBUILD_LOG
 sw_vers
