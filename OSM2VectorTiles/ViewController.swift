c61338846c17ebdfb3460c10574c6e7e213d1970
RobLabs.com <roblabs@users.noreply.github.com>
2021-01-30 13:43:17 -0800

    Refactor `viewDidLoad()`

--- OSM2VectorTiles/ViewController.swift
+++ OSM2VectorTiles/ViewController.swift
@@ -17,18 +17,9 @@ class ViewController: UIViewController {
     @IBOutlet var mapView: MGLMapView!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
         mapView.logoView.isHidden = true;
-        
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
