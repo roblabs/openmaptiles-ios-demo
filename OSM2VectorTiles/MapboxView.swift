1f774754a1b83b6905ba9c68d13d71f17e562ce1
RobLabs.com <roblabs@users.noreply.github.com>
2021-06-17 09:39:01 -0700

    Add MapView OSLog Extensions from https://github.com/roblabs/mapview-log-extensions

--- OSM2VectorTiles/MapboxView.swift
+++ OSM2VectorTiles/MapboxView.swift
@@ -1,22 +1,25 @@
 import SwiftUI
 import Mapbox
+import OSLog
+import MapViewOSLogExtensions
 
 struct MapboxView: UIViewRepresentable {
-
+    var oslog = OSLog(subsystem: OSLog.subsystemMapview, category: OSLog.categoryMapview)
     private let mapView: MGLMapView = MGLMapView(frame: .zero, styleURL: MGLStyle.streetsStyleURL)
     
     // MARK: - Configuring UIViewRepresentable protocol
     
     func makeUIView(context: Context) -> MGLMapView {
+        OSLog.mapView(.event, "frame: \(mapView.frame)")
         mapView.delegate = context.coordinator
         mapView.logoView.isHidden = true
         return mapView
     }
     
     func updateUIView(_ uiView: MGLMapView, context: Context) {
-
         let localStyle = "asset://styles/geography-class-local.json"
         let _ = setStyle(localStyle)
+        OSLog.mapView(.event, "style: \(mapView.styleURL)")
     }
     
     func makeCoordinator() -> MapboxView.Coordinator {
@@ -48,29 +51,38 @@ struct MapboxView: UIViewRepresentable {
         var control: MapboxView
         
         init(_ control: MapboxView) {
+            OSLog.mapView(.event, "Coordinator init")
             self.control = control
         }
-        
-        func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
-            
-            let coordinates = [
-                CLLocationCoordinate2D(latitude: 37.791329, longitude: -122.396906),
-                CLLocationCoordinate2D(latitude: 37.791591, longitude: -122.396566),
-                CLLocationCoordinate2D(latitude: 37.791147, longitude: -122.396009),
-                CLLocationCoordinate2D(latitude: 37.790883, longitude: -122.396349),
-                CLLocationCoordinate2D(latitude: 37.791329, longitude: -122.396906),
-            ]
-            
-            let buildingFeature = MGLPolygonFeature(coordinates: coordinates, count: 5)
-            let shapeSource = MGLShapeSource(identifier: "buildingSource", features: [buildingFeature], options: nil)
-            mapView.style?.addSource(shapeSource)
-            
-            let fillLayer = MGLFillStyleLayer(identifier: "buildingFillLayer", source: shapeSource)
-            fillLayer.fillColor = NSExpression(forConstantValue: UIColor.blue)
-            fillLayer.fillOpacity = NSExpression(forConstantValue: 0.5)
-            
-            mapView.style?.addLayer(fillLayer)
 
+        /// Log events 🦮100. WillStartLoadingMap
+        func mapViewWillStartLoadingMap(_ mapView: MGLMapView) {
+            OSLog.mapView(.event, OSLog.mapEvents.WillStartLoadingMap.rawValue)
+        }
+
+        /// Log events 🦮1. WillStartRenderingMap
+        func mapViewWillStartRenderingMap(_ mapView: MGLMapView) {
+            OSLog.mapView(.event, OSLog.mapEvents.WillStartRenderingMap.rawValue)
+        }
+
+        /// Log events 🦮2. DidFinishLoadingStyle
+        func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+            OSLog.mapView(.event, OSLog.mapEvents.DidFinishLoadingStyle.rawValue)
+        }
+        
+        /// Log events 🦮3. DidFinishRenderingMap
+        func mapViewDidFinishRenderingMap(_ mapView: MGLMapView, fullyRendered: Bool) {
+            OSLog.mapView(.event, OSLog.mapEvents.DidFinishRenderingMap.rawValue)
+        }
+
+        /// Log events 🦮4. DidFinishLoadingMap
+        func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+            OSLog.mapView(.event, OSLog.mapEvents.DidFinishLoadingMap.rawValue)
+        }
+        
+        /// Log events 🦮5. DidBecomeIdle
+        func mapViewDidBecomeIdle(_ mapView: MGLMapView) {
+            OSLog.mapView(.event, OSLog.mapEvents.DidBecomeIdle.rawValue)
         }
         
         func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
@@ -80,9 +92,5 @@ struct MapboxView: UIViewRepresentable {
         func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
             return true
         }
-        
     }
-    
 }
-
-
