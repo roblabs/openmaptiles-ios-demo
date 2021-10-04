c555241d5e95f41ab082e49c38cbaec28a49f56a
RobLabs.com <roblabs@users.noreply.github.com>
2021-10-04 08:38:19 -0700

    String extension to print out path information to the Mapbox `cache.db`.

---
+++ OSM2VectorTiles/String+mapboxCacheDBPath.swift
@@ -0,0 +1,25 @@
+//
+//  String+mapboxCacheDBPath.swift
+//  GeographyClass
+//
+//  Copyright © 2021 ePi Rational, Inc.. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    /// Returns the path to either the user’s or application’s home directory
+    static let HOME_DIRECTORY = NSHomeDirectory()
+    
+    /// Application Path & Bundle Identifier
+    static let APPLICATION_SUPPORT_PATH = "/Library/Application Support/\(Bundle.main.bundleIdentifier ?? "bundleIdentifier")"
+    
+    /// Hidden folder  of path to `cache.db`
+    static let CACHE_DB_PATH = "/.mapbox"
+
+    /// String extension to print out path information to the Mapbox `cache.db`.
+    /// To open the cache database on macOS, use
+    /// [DB Browser for SQLite](https://sqlitebrowser.org),  *an open source, freeware visual tool used to create, design and edit SQLite database files.*
+    var mapboxCacheDBPath: String { return .HOME_DIRECTORY + .APPLICATION_SUPPORT_PATH + .CACHE_DB_PATH}
+}
